1c1e9f847e0ceb401c26cdc61ce9bdf8072f3ee1
aperfer126 <aperfer126@users.noreply.github.com>
2026-06-14 20:12:06 +0200

    Configuración para Vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
+  nitro: {
+    preset: 'vercel',
+  },
   future: {
     compatibilityVersion: 4,
   },
